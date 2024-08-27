14d1fa6f26b1603bf58ae3c94e7d98c78bbfdb7e
Park Sang Min <sm010422@naver.com>
2024-08-27 17:05:41 +0900

    add vim-visual-mulit plugins

---
+++ lua/plugins/visual-multi.lua
@@ -0,0 +1,12 @@
+-- lazy --
+return {
+  {
+    "mg979/vim-visual-multi",
+    branch = "master",
+    init = function()
+      vim.g.VM_maps = {
+        ["Find Under"] = "<C-d>",
+      }
+    end,
+  },
+}
