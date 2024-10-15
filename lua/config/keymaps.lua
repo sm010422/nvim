00a3df731a06b05bb72d2f395e1ceb6b5cad6995
Park Sang Min <sm010422@naver.com>
2024-10-15 23:55:26 +0900

    keymaps.lua & lazy-lock.lua & lazyvim.json edit

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,4 +2,11 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.set("n", "<leader>a", require("lspimport").import, { noremap = true })
+-- vim.keymap.set("n", "<leader>A", require("lspimport").import, { noremap = true })
+-- keymaps.lua
+
+-- 기존의 vim.keymap.set을 사용하여 keybinding에 설명 추가
+vim.keymap.set("n", "<leader>A", require("lspimport").import, { noremap = true, silent = true, desc = "LSP Import" })
+
+-- 만약 다른 keybinding이 더 필요하다면 여기에 추가할 수 있습니다
+-- 예: vim.keymap.set("n", "<leader>x", "function_name", { noremap = true, silent = true, desc = "Description" })
