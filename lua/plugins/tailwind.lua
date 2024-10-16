7c1a7979f3a049222c11a81b40532dd1993890b9
Park Sang Min <sm010422@naver.com>
2024-10-16 16:24:15 +0900

    tailwind.lua -> rgb & rgba enable edit and hsl enable edit

--- lua/plugins/tailwind.lua
+++ lua/plugins/tailwind.lua
@@ -12,6 +12,10 @@ return {
     opts = {
       user_default_options = {
         tailwind = true,
+        rgb_fn = true,
+        hsl_fn = true,
+        css = true,
+        css_fn = true,
       },
     },
   },
