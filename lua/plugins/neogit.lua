dc16d9fd71ca81ed03fe405999cab243074e9f26
Park Sang Min <sm010422@naver.com>
2024-11-06 15:45:46 +0900

    neogit = nvim-telescope.nvim and fzf-lua aready installed and error fix

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -6,8 +6,6 @@ return {
       "sindrets/diffview.nvim", -- optional - Diff integration
 
       -- Only one of these is needed.
-      "nvim-telescope/telescope.nvim", -- optional
-      "ibhagwan/fzf-lua", -- optional
       "echasnovski/mini.pick", -- optional
     },
     config = true,
