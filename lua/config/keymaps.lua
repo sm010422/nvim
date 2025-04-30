53f399b1a9bf4b170cd5246b11fff6896027b562
Park Sang Min <sm010422@naver.com>
2025-04-30 10:40:05 +0900

    lspimport keybinding change "<leader>A" to "<leader>I"

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,7 +6,7 @@
 -- keymaps.lua
 
 -- 기존의 vim.keymap.set을 사용하여 keybinding에 설명 추가
-vim.keymap.set("n", "<leader>A", require("lspimport").import, { noremap = true, silent = true, desc = "LSP Import" })
+vim.keymap.set("n", "<leader>I", require("lspimport").import, { noremap = true, silent = true, desc = "LSP Import" })
 
 -- 만약 다른 keybinding이 더 필요하다면 여기에 추가할 수 있습니다
 -- 예: vim.keymap.set("n", "<leader>x", "function_name", { noremap = true, silent = true, desc = "Description" })
