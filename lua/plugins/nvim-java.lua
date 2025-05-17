d0753e5190904c26ece9f36d3fe752dc7668f403
Park Sang Min <sm010422@naver.com>
2025-05-17 17:11:11 +0900

    jdtls.lua change to all comment and nvim-java.lus edit

--- lua/plugins/nvim-java.lua
+++ lua/plugins/nvim-java.lua
@@ -1,1 +1,42 @@
-return { "nvim-java/nvim-java" }
+return {
+  'nvim-java/nvim-java',
+  enabled = false, -- plus
+  config = false,
+  dependencies = {
+  {
+        'neovim/nvim-lspconfig',
+        opts = {
+          servers = {
+            jdtls = {
+              handlers = {
+                ["language/status"] = function(_, _)
+                  -- 아무 동작도 하지 않음
+                end,
+                ['$/progress'] = function(_, result, ctx) 
+
+                end,
+              },
+              capabilities = vim.lsp.protocol.make_client_capabilities(),
+            },
+          },
+          setup = {
+            jdtls = function()
+              require('java').setup({
+                jdk = {
+                  auto_install = false,
+                },
+                notifications = {
+                  dap = false,
+                },
+              })
+
+              require('lspconfig').jdtls.setup({})
+
+              -- return true will skip mason-lspconfig from setting up jdtls
+              return true
+            end,
+          },
+        },
+      },
+  },
+}
