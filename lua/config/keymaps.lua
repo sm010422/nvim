5135df2f6b4b40275da8cad3fead34e722fc817f
Park Sang Min <sm010422@naver.com>
2025-05-27 17:23:19 +0900

    lua/config/keymaps.lua copilot enable/disable keymaps setting

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -10,3 +10,15 @@ vim.keymap.set("n", "<leader>I", require("lspimport").import, { noremap = true, 
 
 -- 만약 다른 keybinding이 더 필요하다면 여기에 추가할 수 있습니다
 -- 예: vim.keymap.set("n", "<leader>x", "function_name", { noremap = true, silent = true, desc = "Description" })
+
+-- Copilot 끄기
+vim.keymap.set("n", "<leader>ba", function()
+  vim.cmd("Copilot disable")
+  vim.notify("Copilot Disabled", vim.log.levels.INFO)
+end, { desc = "Copilot Disable" })
+
+-- Copilot 켜기
+vim.keymap.set("n", "<leader>bA", function()
+  vim.cmd("Copilot enable")
+  vim.notify("Copilot Enabled", vim.log.levels.INFO)
+end, { desc = "Copilot Enable" })
