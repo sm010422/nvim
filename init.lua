f3e73b9e98d5e70c1c20eb9f3142ddbd48ea9fae
Park Sang Min <sm0104224@gmail.com>
2025-08-26 12:07:16 +0900

    feat(nvim): Tokyonight(night) 팔레트 런타임 패치로 배경 톤 통일

--- init.lua
+++ init.lua
@@ -19,3 +19,20 @@ end
 
 sepfix()
 vim.api.nvim_create_autocmd("ColorScheme", { callback = sepfix })
+
+vim.o.termguicolors = true
+
+local ok, tn = pcall(require, "tokyonight")
+if ok then
+  -- style에 맞는 팔레트 모듈을 불러와 '테이블 자체'를 수정
+  local palette = require("tokyonight.colors.night")  -- <-- night 스타일이면 night
+  palette.bg       = "#0a0a0f"
+  palette.bg_dark  = "#05050a"
+  palette.bg_dark1 = "#000000"
+
+  tn.setup({
+    style = "night",
+    transparent = false,
+  })
+  vim.cmd.colorscheme("tokyonight")
+end
