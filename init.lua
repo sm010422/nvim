370488da4ee4d33b8c4e4a252532c5f8f55e2e02
Park Sang Min <sm0104224@gmail.com>
2025-08-26 11:24:27 +0900

    feat(nvim): 창 경계선 가시성 개선 (WinSeparator)

--- init.lua
+++ init.lua
@@ -8,3 +8,14 @@ vim.api.nvim_create_autocmd("BufReadPre", {
     vim.b.autoformat = false
   end,
 })
+
+vim.o.termguicolors = true
+
+local function sepfix()
+  local hl = { fg = "#3b4261", bg = "NONE", bold = false }
+  vim.api.nvim_set_hl(0, "WinSeparator", hl)
+  vim.api.nvim_set_hl(0, "VertSplit", { link = "WinSeparator" })
+end
+
+sepfix()
+vim.api.nvim_create_autocmd("ColorScheme", { callback = sepfix })
