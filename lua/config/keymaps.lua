5c3949ce641a1d73e6ba159359cbf23b6484161b
Park Sang Min <sm0104224@gmail.com>
2025-09-10 13:04:26 +0900

    feat: keymaps.lua edit -> clean code & comment for java-creator-nvim

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,10 +5,6 @@
 -- vim.keymap.set("n", "<leader>A", require("lspimport").import, { noremap = true })
 -- keymaps.lua
 
--- 기존의 vim.keymap.set을 사용하여 keybinding에 설명 추가
-vim.keymap.set("n", "<leader>I", require("lspimport").import, { noremap = true, silent = true, desc = "LSP Import" })
-
--- 만약 다른 keybinding이 더 필요하다면 여기에 추가할 수 있습니다
 -- 예: vim.keymap.set("n", "<leader>x", "function_name", { noremap = true, silent = true, desc = "Description" })
 
 -- Copilot 끄기
@@ -23,6 +19,7 @@ vim.keymap.set("n", "<leader>bA", function()
   vim.notify("Copilot Enabled", vim.log.levels.INFO)
 end, { desc = "Copilot Enable" })
 
+-- java-creator-nvim 
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "java",
   callback = function(a)
