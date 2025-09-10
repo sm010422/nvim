41886c4e5606fcf89d8793ee58a12bc411ecf81e
Park Sang Min <sm0104224@gmail.com>
2025-09-10 13:09:20 +0900

    feat: nvim_lspimport.lua edit (python import) -> only activate when python file open

--- lua/plugins/nvim_lspimport.lua
+++ lua/plugins/nvim_lspimport.lua
@@ -1,4 +1,23 @@
-return{
-  {"stevanmilic/nvim-lspimport"}
-
-  }
+return {
+  {
+    "stevanmilic/nvim-lspimport",
+    ft = "python",
+    config = function()
+      local grp = vim.api.nvim_create_augroup("LspImportPythonKeymap", { clear = true })
+      vim.api.nvim_create_autocmd("FileType", {
+        group = grp,
+        pattern = "python",
+        callback = function(args)
+          vim.keymap.set("n", "<leader>I", function()
+            require("lspimport").import()
+          end, {
+            noremap = true,
+            silent = true,
+            desc = "Python LSP Import",
+            buffer = args.buf, -- 해당 파이썬 버퍼에만
+          })
+        end,
+      })
+    end,
+  },
+}
