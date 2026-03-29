26d8512c3574bd95d738a2d37c0813de4056196d
Park Sang Min <sm0104224@gmail.com>
2026-03-29 22:32:45 +0900

    feat(plugin-edgy): dbui plane drage enabled

--- lua/plugins/edgy.lua
+++ lua/plugins/edgy.lua
@@ -2,34 +2,35 @@ return {
   {
     "folke/edgy.nvim",
     event = "VeryLazy",
-    opts = function(_, opts)
-      -- 1. 미끄러지는 창 애니메이션 끄기
-      opts.animate = {
-        enabled = false,
-      }
+    opts = function()
+      return {
+        -- 1. 애니메이션 비활성화
+        animate = { enabled = false },
 
-      -- 2. 오른쪽 패널 덮어쓰기 (기본 설정 무시하고 깔끔하게 초기화)
-      opts.right = {
-        {
-          title = "Outline",
-          ft = "Outline",
-          pinned = true,
+        -- 2. 왼쪽 패널 (Database)
+        left = {
+          {
+            title = "Database",
+            ft = "dbui",
+            pinned = true,
+            size = { width = 40 }, 
+          },
         },
-        {
-          title = "Aerial",
-          ft = "aerial",
-          pinned = true,
-        },
-      }
 
-      -- 3. 왼쪽 패널 덮어쓰기 (Database만 단독으로 배치)
-      opts.left = {
-        {
-          title = "Database",
-          ft = "dbui",
-          pinned = true,
+        -- 3. 오른쪽 패널
+        right = {
+          { title = "Outline", ft = "Outline", pinned = true, size = { width = 30 } },
+          { title = "Aerial", ft = "aerial", pinned = true, size = { width = 30 } },
+        },
+
+        options = {
+        },
+
+        wo = {
+          winfixwidth = false,
+          winfixheight = false,
         },
       }
-    end
-  }
+    end,
+  },
 }
