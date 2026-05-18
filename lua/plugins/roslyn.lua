85a1fe3a85a304aee9dd29bc822de62018f5afff
Park Sang Min <sm0104224@gmail.com>
2026-05-18 16:51:33 +0900

    feat: update roslyn.lua for c#

--- lua/plugins/roslyn.lua
+++ lua/plugins/roslyn.lua
@@ -2,6 +2,8 @@ return {
   {
     "seblyng/roslyn.nvim",
     ft = "cs",
+    ---@module 'roslyn.config'
+    ---@type RoslynNvimConfig
     opts = {
       silent = false,
       filewatching = "off",
@@ -9,7 +11,8 @@ return {
         razor = { enabled = false },
       },
     },
-    config = function(_, opts)
+    init = function()
+      -- LSP 클라이언트 시작 전에 settings 적용 (타이밍 보장)
       vim.lsp.config("roslyn", {
         settings = {
           ["csharp|code_lens"] = {
@@ -21,6 +24,8 @@ return {
           },
         },
       })
+    end,
+    config = function(_, opts)
       require("roslyn").setup(opts)
 
       -- info 레벨 가상 텍스트 숨기기
@@ -29,6 +34,63 @@ return {
           severity = { min = vim.diagnostic.severity.WARN },
         },
       })
+    end,
+  },
+  -- fzf-lua extra가 gd/gr을 FzfLua lsp_definitions로 전역 오버라이드함.
+  -- roslyn이 roslyn-source-generated:// URI를 반환할 때 fzf-lua가 처리 못해 빈 버퍼가 생김.
+  -- Snacks.keymap은 ID 역순(최신 우선)으로 키맵을 적용하므로,
+  -- servers["*"] 보다 나중에 등록되는 servers.roslyn 키맵이 자동으로 우선순위를 가짐.
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        roslyn = {
+          -- roslyn은 plugin/roslyn.lua에서 직접 vim.lsp.enable()을 호출하므로
+          -- lspconfig가 다시 enable하지 않도록 막음
+          enabled = false,
+          keys = {
+            -- gd: roslyn-source-generated:// URI는 fzf-lua가 처리 못하므로 vim.lsp.buf 사용
+            { "gd", vim.lsp.buf.definition, desc = "Goto Definition", has = "definition" },
+            -- gr/gI/gy: 프로젝트 경로에 '#'이 포함되면 fzf-lua가 file:// URI 생성 시
+            -- '#'을 fragment로 해석해 경로가 잘림 → _uri=false로 오버라이드
+            {
+              "gr",
+              function()
+                require("fzf-lua").lsp_references({
+                  jump1 = true, ignore_current_line = true, _uri = false,
+                })
+              end,
+              desc = "References", nowait = true,
+            },
+            {
+              "gI",
+              function()
+                require("fzf-lua").lsp_implementations({
+                  jump1 = true, ignore_current_line = true, _uri = false,
+                })
+              end,
+              desc = "Goto Implementation",
+            },
+            {
+              "gy",
+              function()
+                require("fzf-lua").lsp_typedefs({
+                  jump1 = true, ignore_current_line = true, _uri = false,
+                })
+              end,
+              desc = "Goto T[y]pe Definition",
+            },
+          },
+        },
+      },
+    },
+  },
+
+  {
+    "GustavEikaas/easy-dotnet.nvim",
+    dependencies = { "nvim-lua/plenary.nvim", "folke/snacks.nvim" },
+    config = function()
+      require("easy-dotnet").setup()
     end,
   },
 }
