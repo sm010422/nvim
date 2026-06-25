09439a080220f395dc4cc2fac4bb6d85be19760b
Park Sang Min <sm0104224@gmail.com>
2026-06-25 11:01:18 +0900

    feat: config = function() setup

--- lua/plugins/refactoring.lua
+++ lua/plugins/refactoring.lua
@@ -2,10 +2,15 @@
 -- lazy.nvim이 스펙을 병합하므로 동작은 하지만, 한 파일로 통합하는 게 깔끔함.
 -- coding.lua의 정의를 이 파일로 옮기거나, 이 파일을 삭제하고 coding.lua 하나로 관리 권장.
 return {
-  "ThePrimeagen/refactoring.nvim",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-treesitter/nvim-treesitter",
-    "lewis6991/async.nvim",
-  },
+  {
+    "ThePrimeagen/refactoring.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+      "lewis6991/async.nvim",
+    },
+    config = function()
+      require("refactoring").setup()
+    end,
+  }
 }
